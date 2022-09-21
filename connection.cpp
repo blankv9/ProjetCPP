6e4439d8e2e294aecf8d437bd749be42eccd71d2
Ahmed <ahmed.arfaoui1@esprit.tn>
2022-09-21 00:00:50 -0700

    connection replace

--- connection.cpp
+++ connection.cpp
@@ -7,8 +7,8 @@ bool Connection::ouvrirConnexion()
 {bool test=false;
 QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
 db.setDatabaseName("Projet");//inserer le nom de la source de données ODBC
-db.setUserName("wajih");//inserer nom de l'utilisateur
-db.setPassword("wajih");//inserer mot de passe de cet utilisateur
+db.setUserName("Ahmed");//inserer nom de l'utilisateur
+db.setPassword("esprit22");//inserer mot de passe de cet utilisateur
 if (db.open())
     test=true;
 return  test;
